fcf00366e84f12723e476a38a20d0e1636110821
Nicole kibe <nicolekibe822@gmail.com>
2026-01-03 15:10:37 +0300

    Complete Chatterbox lab implementation

--- server/app.py
+++ server/app.py
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        # Get all messages ordered by created_at ascending
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return make_response(jsonify([msg.to_dict() for msg in messages]), 200)
+    
+    elif request.method == 'POST':
+        # Get JSON data from request
+        data = request.get_json()
+        
+        # Create new message
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return make_response(jsonify(new_message.to_dict()), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    
+    if request.method == 'PATCH':
+        # Get JSON data
+        data = request.get_json()
+        
+        # Update the message body
+        message.body = data['body']
+        
+        db.session.commit()
+        
+        return make_response(jsonify(message.to_dict()), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        
+        return make_response(jsonify({}), 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
